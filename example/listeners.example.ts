f285bdeea0cd56ae73020c6baff80a008f78b636
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-27 14:22:50 +0000

    chore(listeners.example.ts): add type annotations to listener functions for improved type safety

--- example/listeners.example.ts
+++ example/listeners.example.ts
@@ -38,7 +38,8 @@ export class ListenersSetAdapter<
 const listeners = new Listeners(
   false,
   ListenersSetAdapter,
-  (a, b) => console.log('Listener 1', a, b),
+  ((a: number, b: number) => console.log('Listener 1', a, b)) as ListenerFunction<[number, number] | [string, boolean]>,
+  ((a: string, b: boolean) => console.log('Listener 2', a, b)) as ListenerFunction<[string, boolean]>,
   // async (a, b) => {
   //   await new Promise(resolve => setTimeout(resolve, 1000));
   //   console.log('Listener 2', a, b);
